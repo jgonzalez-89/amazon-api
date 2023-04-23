7cd7117dafd41dce78122d7e324810f2d75a2522
jgonzalez-89 <kbcerverjo@gmail.com>
2023-04-23 23:30:21 +0200

    first commit

--- src/api/models.py
+++ src/api/models.py
@@ -1,21 +1,34 @@
 import os
-from sqlalchemy import Column, Integer, String, Float, Date
-from sqlalchemy.orm import class_mapper
+from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey
+from sqlalchemy.orm import class_mapper, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class Producto:
+class Producto(Base):
     __tablename__ = 'productos'
 
     id = Column(Integer, primary_key=True)
-    fecha = Column(Date)
     imagen = Column(String)
     nombre = Column(String)
     distribuidor = Column(String)
-    ASIN=Column(String)
+    ASIN = Column(String)
+    EAN = Column(String)
+
+    precios_historicos = relationship("PrecioHistorico", back_populates="producto")
+
+    def as_dict(self):
+        return {c.key: getattr(self, c.key) for c in class_mapper(self.__class__).columns}
+
+class PrecioHistorico(Base):
+    __tablename__ = 'precios_historicos'
+
+    id = Column(Integer, primary_key=True)
+    producto_id = Column(Integer, ForeignKey('productos.id'))
+    fecha = Column(Date)
     precio = Column(Float)
-    EAN= Column(String)
+
+    producto = relationship("Producto", back_populates="precios_historicos")
 
     def as_dict(self):
         return {c.key: getattr(self, c.key) for c in class_mapper(self.__class__).columns}
--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 import os
-import json
+import ujson
 from dotenv import load_dotenv
-from api.models import Producto
+# from api.models import Producto
 from flask import Flask, jsonify, render_template, request
 from datetime import datetime, date
 from flask_cors import CORS
@@ -9,6 +9,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy import desc
 import pandas as pd
+import math
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, class_mapper
+from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey, Numeric
+from sqlalchemy.orm import selectinload
+
 
 load_dotenv()
 
@@ -19,27 +26,64 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
-class Product(db.Model, Producto):
-    pass
+class Producto(db.Model):
+    __tablename__ = 'productos'
+
+    id = Column(Integer, primary_key=True)
+    imagen = Column(String(255))
+    nombre = Column(String(255))
+    distribuidor = Column(String(255))
+    ASIN = Column(String(255), index=True)
+    EAN = Column(Numeric(asdecimal=False), nullable=True, index=True)
+
+    precios_historicos = relationship(
+        "PrecioHistorico", back_populates="producto")
+
+    def as_dict(self):
+        precios_historicos = [precio.as_dict() for precio in self.precios_historicos]
+        return {
+            'id': self.id,
+            'imagen': self.imagen,
+            'nombre': self.nombre,
+            'distribuidor': self.distribuidor,
+            'ASIN': self.ASIN,
+            'EAN': None if self.EAN is None or (isinstance(self.EAN, float) and math.isnan(self.EAN)) else float(self.EAN),
+            'precios_historicos': precios_historicos
+        }
 
 
-@app.route("/")
-def sitemap():
-    today = date.today()
-    last_five_dates = Product.query.distinct(
-        Product.fecha).order_by(desc(Product.fecha)).limit(5).all()
-    last_five_dates = [p.fecha for p in last_five_dates]
-    return render_template("index.html", today=today, last_five_dates=last_five_dates)
+class PrecioHistorico(db.Model):
+    __tablename__ = 'precios_historicos'
 
+    id = Column(Integer, primary_key=True)
+    fecha = Column(Date, index=True)
+    precio = Column(Float)
+
+    producto_id = Column(Integer, ForeignKey('productos.id'))
+    producto = relationship("Producto", back_populates="precios_historicos")
+
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'fecha': self.fecha.isoformat(),
+            'precio': float(self.precio),
+            # 'producto_id': self.producto_id
+        }
+
+
+from flask import request
 
 @app.route("/productos", methods=["GET"])
 def get_products():
     try:
-        products = Product.query.all()
-        return jsonify([p.as_dict() for p in products])
+        page = int(request.args.get('page', 1))
+        per_page = int(request.args.get('per_page', 100))
+        products = Producto.query.paginate(page=page, per_page=per_page, error_out=False)
+        return jsonify([p.as_dict() for p in products.items])
     except Exception as e:
         print("Error al obtener productos:", e)
         return jsonify({"error": "Error al obtener productos"}), 500
+
 
 
 @app.route('/fecha/<string:fecha>', methods=['GET'])
@@ -50,8 +94,24 @@ def get_productos_por_fecha(fecha):
         return jsonify({"error": "Formato de fecha inválido"}), 400
 
     try:
-        productos = Product.query.filter_by(fecha=fecha_obj).all()
-        return jsonify([p.as_dict() for p in productos])
+        productos = (
+            Producto.query
+            .options(selectinload(Producto.precios_historicos))
+            .join(PrecioHistorico)
+            .filter(PrecioHistorico.fecha == fecha_obj)
+            .all()
+        )
+        result = []
+        for producto in productos:
+            precio = next(
+                (ph for ph in producto.precios_historicos if ph.fecha == fecha_obj), None)
+            if precio is not None:
+                result.append({
+                    **producto.as_dict(),
+                    "precio": precio.precio,
+                    "fecha": precio.fecha.strftime("%Y-%m-%d")
+                })
+        return jsonify(result)
     except Exception as e:
         print("Error al obtener productos por fecha:", e)
         return jsonify({"error": "Error al obtener productos por fecha"}), 500
@@ -60,49 +120,56 @@ def get_productos_por_fecha(fecha):
 @app.route('/file', methods=['POST'])
 def upload_json():
     if 'file' in request.files:
-        # Si se ha enviado un archivo
         file = request.files['file']
         if file.filename == '':
             return jsonify({'error': 'No se seleccionó ningún archivo'}), 400
 
-        # Leer el archivo JSON y convertirlo en un objeto Python
-        file_content = json.load(file)
+        file_content = ujson.load(file)
 
     elif request.is_json:
-        # Si se ha enviado un objeto JSON en crudo
         file_content = request.get_json()
 
     else:
         return jsonify({'error': 'No se encontró el archivo o el JSON en crudo'}), 400
 
-    # Convertir el objeto Python en una lista si no es una lista
     if not isinstance(file_content, list):
         file_content = [file_content]
 
-    # Convertir la lista de objetos Python en un DataFrame de Pandas
     data = pd.DataFrame.from_dict(file_content)
     records = data.to_dict(orient='records')
 
     try:
         for record in records:
             fecha_obj = datetime.strptime(record['fecha'], "%d-%m-%Y").date()
-            producto = Product(
+            producto_existente = Producto.query.filter_by(
+                ASIN=record['ASIN'], distribuidor=record['distribuidor']).first()
+
+            if producto_existente:
+                producto = producto_existente
+            else:
+                producto = Producto(
+                    nombre=record['nombre'],
+                    distribuidor=record['distribuidor'],
+                    ASIN=record['ASIN'],
+                    imagen=record['imagen'],
+                    EAN=record['EAN']
+                )
+                db.session.add(producto)
+                db.session.flush()
+
+            precio_historico = PrecioHistorico(
                 fecha=fecha_obj,
-                nombre=record['nombre'],
-                distribuidor=record['distribuidor'],
-                ASIN=record['ASIN'],
                 precio=record['precio'],
-                imagen=record['imagen'],
-                EAN=record['EAN']
+                producto_id=producto.id
             )
-            db.session.add(producto)
+            db.session.add(precio_historico)
+
         db.session.commit()
         return jsonify({'message': 'JSON procesado y añadido'})
     except Exception as e:
         print("Error al procesar y guardar los datos del JSON:", e)
         db.session.rollback()
         return jsonify({'error': 'Error al procesar y guardar los datos del JSON'}), 500
-
 
 
 @app.route('/fecha/<string:fecha>', methods=['DELETE'])
@@ -113,7 +180,7 @@ def delete_productos_por_fecha(fecha):
         return jsonify({"error": "Formato de fecha inválido"}), 400
 
     try:
-        productos = Product.query.filter_by(fecha=fecha_obj).all()
+        productos = Producto.query.filter_by(fecha=fecha_obj).all()
         if not productos:
             return jsonify({'error': 'No se encontraron productos para la fecha especificada'}), 404
 
